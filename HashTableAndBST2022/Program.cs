9413fc17e757c618c00055333893e074d14cce3b
Ankit <ankitcool4370@gmail.com>
2022-12-04 14:59:05 +0530

    Performed UC 4

--- HashTableAndBST2022/Program.cs
+++ HashTableAndBST2022/Program.cs
@@ -11,8 +11,19 @@ namespace HashTableAndBST2022
         public static void Main()
         {
             BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+            binarySearch.Insert(3);
+            binarySearch.Insert(11);
+            binarySearch.Insert(16);
+            binarySearch.Insert(22);
             binarySearch.Insert(30);
+            binarySearch.Insert(40);
+            binarySearch.Insert(56);
             binarySearch.Insert(70);
+            binarySearch.Insert(60);
+            binarySearch.Insert(96);
+            binarySearch.Insert(65);
+            binarySearch.Insert(63);
+            binarySearch.Insert(67);
             binarySearch.GetSize();
             binarySearch.Display();
             Console.ReadLine();
